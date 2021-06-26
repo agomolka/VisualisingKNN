f0e8b121ff23b5caf5c26f8f542b848a8d673d4c
Aleksandra Rozwadowska <axrozwadowska@gmail.com>
2021-06-26 22:47:26 +0200

    Change visualisation for diffrent variables in visualisation

--- visualisation.py
+++ visualisation.py
@@ -19,15 +19,23 @@ class KnnVisualisation2d:
         self.k = k
 
     def knn_comparison(self):
-        x = self.data[["X", "Y"]].values
-        y = self.data["class"].astype(int).values
+        data_columns_values_list = self.data.columns.values.tolist()
+        x = self.data.iloc[:, [0, 1]].values
+        y = self.data.iloc[:, [len(data_columns_values_list) - 1]].values.ravel()
+
+        # x = self.data[["X", "Y"]].values
+        # y = self.data["class"].astype(int).values
         clf = neighbors.KNeighborsClassifier(n_neighbors=self.k)
         clf.fit(x, y)
         # Plotting decision region
         plot_decision_regions(x, y, clf=clf, legend=2)
+
+
+
+        unique = self.data.columns
         # Adding axes annotations
-        plt.xlabel("X")
-        plt.ylabel("Y")
+        plt.xlabel(unique[0])
+        plt.ylabel(unique[1])
         plt.title("k-NN with k=" + str(self.k))
         name = f'img/knn' + str(self.k) + '.png'
         plt.savefig(name)
